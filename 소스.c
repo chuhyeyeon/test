062b7968b20540c3685a4d0264eb1bd9a2d70820
unknown <chuhyeyeon0221@gmail.com>
2023-01-07 15:45:27 +0900

    0107

---
+++ 소스.c
@@ -0,0 +1,25 @@
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	printf("hello Everybody");
+//	printf("%d", 1234);
+//	printf("%d %d", 10, 20);
+//	return 0;
+//}   
+
+#include <stdio.h>
+
+int main(void)
+{
+	int num1, num2;  
+	int num3 = 30, num4 = 40;
+
+	printf("num1: %d, num2: %d \n");
+	num1 = 10;
+	num2 = 20;
+
+	printf("num1: %d, num2: %d \n", num1, num2);
+	printf("num3: %d, num4: %d \n", num3, num4);
+	return 0;
+}
